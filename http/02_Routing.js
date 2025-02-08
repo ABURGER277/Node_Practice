f6226c05a1db8827b1aead60267fa5cfd51ce68c
aburger <aburger277@gmail.com>
2025-02-08 17:53:34 +0900

    Router 수정(UTF-8추가)

--- http/02_Routing.js
+++ http/02_Routing.js
@@ -1,3 +1,4 @@
+// 기본적인 Router 구현
 const http = require('http');
 
 const server = http.createServer((req, res) => {
@@ -5,13 +6,15 @@ const server = http.createServer((req, res) => {
     console.log(`요청 URL: ${req.url}, 요청 메서드: ${req.method}`);
 
     if (req.url === '/' && req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        // 한글이라 charset=UTF-8 추가
+        // '='에 띄어쓰기 하면 안됨.
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' });
         res.end('홈페이지입니다!');
     } else if (req.url === '/about' && req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' });
         res.end('소개 페이지입니다!');
     } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.writeHead(404, { 'Content-Type': 'text/plain; charset=UTF-8' });
         res.end('페이지를 찾을 수 없습니다.');
     }
 });
@@ -20,4 +23,14 @@ server.listen(3000, () => {
     console.log('서버가 http://localhost:3000 에서 실행 중');
 });
 
-// localhost:3000/about 등으로 라우팅 테스트 가능
+// 서버가 http://localhost:3000 에서 실행 중
+
+// 브라우저로 localhost:3000 접속
+
+// 요청 URL: /, 요청 메서드: GET
+// 요청 URL: /favicon.ico, 요청 메서드: GET
+// 요청 URL: /, 요청 메서드: GET
+
+// localhost:3000/about 접속
+
+// 요청 URL: /about, 요청 메서드: GET
